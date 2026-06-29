e8464c60b05f68192d67ff6cf26bacf0d3df5004
thutrang0410 <trhieuforword@gmail.com>
2026-06-29 15:01:04 +0700

    Update vietbot-aio.sh

--- vietbot-aio.sh
+++ vietbot-aio.sh
@@ -146,7 +146,7 @@ hide_bloatware() {
 
 hide_bloatwares() {
     log_info "Vô hiệu hóa bloatware..."
-    local apps="airskill exceptionreporter ijetty netctl otaservice productiontest bugreport"
+    local apps="airskill exceptionreporter ijetty netctl systemtool otaservice productiontest bugreport"
     for app in $apps; do
         "$ADB" -s "$ADB_DEVICE" shell /system/bin/pm hide "com.phicomm.speaker.$app" >/dev/null 2>&1
     done
